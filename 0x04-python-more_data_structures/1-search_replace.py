c5a7c24a888e3a2e307efda049dcab062612b8cf
dinarwanjiru <dinarwanjiru77@gmail.com>
2023-12-06 12:19:06 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-'''function that replaces all occurrences of an 
-element by another in a new list.'''
-
 
 def search_replace(my_list, search, replace):
     if len(my_list) == 0:
