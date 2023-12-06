b37a9b39ed6f7884ee0cd69f3cf3e5bf2ecfe858
dinarwanjiru <dinarwanjiru77@gmail.com>
2023-12-06 11:05:16 -0800

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/user/bin/python3
+'''function that replaces all occurrences of an element by another in a new list.'''
+
+
+def search_replace(my_list, search, replace):
+    if my_list == 0:
+        return (my_list)
+    new_list = [element if element != search else replace for element in my_list]
+    return (new_list)
