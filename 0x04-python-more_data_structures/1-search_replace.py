e67f7e72cf86ce52a5c847a1ecfa9e0fc74a6bc4
dinarwanjiru <dinarwanjiru77@gmail.com>
2023-12-06 11:36:32 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,6 +6,5 @@ def search_replace(my_list, search, replace):
     if len(my_list) == 0:
         return (my_list)
 
-
     new_list = [element if element != search else replace for element in my_list]
     return (new_list)
