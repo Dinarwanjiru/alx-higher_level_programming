d7fda1139afa9a1ec982121dad2cb2fe66b055b0
dinarwanjiru <dinarwanjiru77@gmail.com>
2024-01-04 17:47:02 +0000

    file0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""empty class that defines a rectangle"""
-class rectangle:
-	"""empty class"""
-	pass
+"""A class that defines a rectangle"""
+
+
+class Rectangle:
+    """An empty class that represents a rectangle"""
+    pass
